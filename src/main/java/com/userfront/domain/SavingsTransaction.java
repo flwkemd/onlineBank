9047aef0f8ce6c2db2637006ae138edcdc89c651
flwkemd <flwkemd@naver.com>
2018-01-23 00:53:08 +0100

    implemented Hibernate Configuration

--- src/main/java/com/userfront/domain/SavingsTransaction.java
+++ src/main/java/com/userfront/domain/SavingsTransaction.java
@@ -3,92 +3,103 @@ package com.userfront.domain;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class SavingsTransaction {
-	private Long id;
-	private Date date;
-	private String description;
-	private String type;
-	private String status;
-	private double amount;
-	private BigDecimal availableBalance;
-	
-	private SavingsAccount savingsAccount;
 
-	public SavingsTransaction() {}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Date date;
+    private String description;
+    private String type;
+    private String status;
+    private double amount;
+    private BigDecimal availableBalance;
 
-	public SavingsTransaction(Date date, String description, String type, String status, double amount,
-			BigDecimal availableBalance, SavingsAccount savingsAccount) {
-		this.date = date;
-		this.description = description;
-		this.type = type;
-		this.status = status;
-		this.amount = amount;
-		this.availableBalance = availableBalance;
-		this.savingsAccount = savingsAccount;
-	}
+    @ManyToOne
+    @JoinColumn(name = "savings_account_id")
+    private SavingsAccount savingsAccount;
 
-	public Long getId() {
-		return id;
-	}
+    public SavingsTransaction() {}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public SavingsTransaction(Date date, String description, String type, String status, double amount, BigDecimal availableBalance, SavingsAccount savingsAccount) {
+        this.date = date;
+        this.description = description;
+        this.type = type;
+        this.status = status;
+        this.amount = amount;
+        this.availableBalance = availableBalance;
+        this.savingsAccount = savingsAccount;
+    }
 
-	public Date getDate() {
-		return date;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public Date getDate() {
+        return date;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public String getStatus() {
-		return status;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	public double getAmount() {
-		return amount;
-	}
+    public String getStatus() {
+        return status;
+    }
 
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
-	public BigDecimal getAvailableBalance() {
-		return availableBalance;
-	}
+    public double getAmount() {
+        return amount;
+    }
 
-	public void setAvailableBalance(BigDecimal availableBalance) {
-		this.availableBalance = availableBalance;
-	}
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 
-	public SavingsAccount getSavingsAccount() {
-		return savingsAccount;
-	}
+    public BigDecimal getAvailableBalance() {
+        return availableBalance;
+    }
 
-	public void setSavingsAccount(SavingsAccount savingsAccount) {
-		this.savingsAccount = savingsAccount;
-	}
-	
+    public void setAvailableBalance(BigDecimal availableBalance) {
+        this.availableBalance = availableBalance;
+    }
+
+    public SavingsAccount getSavingsAccount() {
+        return savingsAccount;
+    }
+
+    public void setSavingsAccount(SavingsAccount savingsAccount) {
+        this.savingsAccount = savingsAccount;
+    }
 }
--- src/main/java/com/userfront/domain/User.java
+++ src/main/java/com/userfront/domain/User.java
@@ -2,126 +2,139 @@ package com.userfront.domain;
 
 import java.util.List;
 
-public class User {
-	private Long userId;
-	private String username;
-	private String password;
-	private String firstName;
-	private String lastName;
-	private String email;
-	private String phone;
-	
-	private boolean enabled=true;
-	
-	private PrimaryAccount primaryAccount;
-	
-	private SavingsAccount savingsAccount;
-	
-	private List<Appointment> appointmentList;
-	
-	private List<Recipient> recipientList;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
-	public Long getUserId() {
-		return userId;
-	}
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
+@Entity
+public class User{
 
-	public String getUsername() {
-		return username;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "userId", nullable = false, updatable = false)
+    private Long userId;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    private String phone;
 
-	public String getPassword() {
-		return password;
-	}
+    private boolean enabled=true;
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    @OneToOne
+    private PrimaryAccount primaryAccount;
 
-	public String getFirstName() {
-		return firstName;
-	}
+    @OneToOne
+    private SavingsAccount savingsAccount;
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Appointment> appointmentList;
 
-	public String getLastName() {
-		return lastName;
-	}
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Recipient> recipientList;
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public Long getUserId() {
+        return userId;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public String getPhone() {
-		return phone;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public boolean isEnabled() {
-		return enabled;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public PrimaryAccount getPrimaryAccount() {
-		return primaryAccount;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public void setPrimaryAccount(PrimaryAccount primaryAccount) {
-		this.primaryAccount = primaryAccount;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public SavingsAccount getSavingsAccount() {
-		return savingsAccount;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setSavingsAccount(SavingsAccount savingsAccount) {
-		this.savingsAccount = savingsAccount;
-	}
+    public String getPhone() {
+        return phone;
+    }
 
-	public List<Appointment> getAppointmentList() {
-		return appointmentList;
-	}
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
-	public void setAppointmentList(List<Appointment> appointmentList) {
-		this.appointmentList = appointmentList;
-	}
+    public List<Appointment> getAppointmentList() {
+        return appointmentList;
+    }
 
-	public List<Recipient> getRecipientList() {
-		return recipientList;
-	}
+    public void setAppointmentList(List<Appointment> appointmentList) {
+        this.appointmentList = appointmentList;
+    }
 
-	public void setRecipientList(List<Recipient> recipientList) {
-		this.recipientList = recipientList;
-	}
+    public List<Recipient> getRecipientList() {
+        return recipientList;
+    }
 
-	@Override
-	public String toString() {
-		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", firstName="
-				+ firstName + ", lastName=" + lastName + ", email=" + email + ", phone=" + phone + ", enabled="
-				+ enabled + "]";
-	}
-	
+    public void setRecipientList(List<Recipient> recipientList) {
+        this.recipientList = recipientList;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public PrimaryAccount getPrimaryAccount() {
+        return primaryAccount;
+    }
+
+    public void setPrimaryAccount(PrimaryAccount primaryAccount) {
+        this.primaryAccount = primaryAccount;
+    }
+
+    public SavingsAccount getSavingsAccount() {
+        return savingsAccount;
+    }
+
+    public void setSavingsAccount(SavingsAccount savingsAccount) {
+        this.savingsAccount = savingsAccount;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
 }
